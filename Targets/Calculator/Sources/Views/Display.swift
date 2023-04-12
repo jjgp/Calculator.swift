59375a3878a0fda8d07270e51deeb20a739ed078
Jason Prasad <jasongprasad@gmail.com>
2023-04-11 21:46:26 -0400

    Fix comment

--- Targets/Calculator/Sources/Views/Display.swift
+++ Targets/Calculator/Sources/Views/Display.swift
@@ -2,7 +2,7 @@ import SwiftUI
 
 struct Display: View {
     var body: some View {
-        // TODO: dynamic display of the textxw
+        // TODO: dynamic display of the text
         Text("0")
             .frame(height: Style.Display.height)
             .padding(.trailing, Style.Display.trailingPadding)
