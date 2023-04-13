fd823014b301b4b09b2bfc5a3596ad798733d372
Jason Prasad <jasongprasad@gmail.com>
2023-04-13 14:32:29 -0400

    Clean up unnecessary HStack
    Add TODO

--- Targets/Calculator/Sources/Views/Display.swift
+++ Targets/Calculator/Sources/Views/Display.swift
@@ -4,12 +4,10 @@ struct Display: View {
     @EnvironmentObject private var solver: Solver
 
     var body: some View {
-        HStack {
-            Text(solver.display)
-                .lineLimit(1)
-                .frame(height: Style.Display.height)
-                .frame(maxWidth: Style.Display.width, alignment: .trailing)
-                .padding([.leading, .trailing], Style.Display.trailingPadding)
-        }
+        Text(solver.display)
+            .lineLimit(1)
+            .frame(height: Style.Display.height)
+            .frame(maxWidth: Style.Display.width, alignment: .trailing)
+            .padding([.leading, .trailing], Style.Display.trailingPadding)
     }
 }
